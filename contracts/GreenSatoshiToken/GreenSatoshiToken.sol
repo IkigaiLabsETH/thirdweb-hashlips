ed6cb1c25bcfc5a55b89f52cd8032cb893d2a285
unbanksytv <unbanksytv@gmail.com>
2023-06-17 23:08:29 +0200

    update

---
+++ contracts/GreenSatoshiToken/GreenSatoshiToken.sol
@@ -0,0 +1,38 @@
+/*
+  
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
+██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
+
+Find any smart contract, and build your project faster: https://www.cookbook.dev
+Twitter: https://twitter.com/cookbook_dev
+Discord: https://discord.gg/WzsfPcfHrk
+
+Find this contract on Cookbook: https://www.cookbook.dev/contracts/GreenSatoshiToken/?utm=code
+*/
+
+// contracts/GreenSatoshiToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./dependencies/ERC20.sol";
+import "./dependencies/ERC20Burnable.sol";
+import "./dependencies/Ownable.sol";
+
+/**
+ * Green Satoshi Token
+ * @author STEPN
+ */
+contract GreenSatoshiToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("GreenSatoshiToken", "GST") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 8;
+    }
+}
